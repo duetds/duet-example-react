d3618f7a9f6719fe1bac9314ea659c5888b9d7cc
lennu <juha-matti.laaksonen@lahitapiola.fi>
2022-03-04 13:08:07 +0200

    Fix sass version and use 0.0.0.0 in playwright

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -11,7 +11,7 @@ const config = {
   },
   webServer: {
     command: 'npm start',
-    url:"http://localhost:3000/",
+    url:"http://0.0.0.0:3000/",
     timeout: 120 * 1000,
     reuseExistingServer: false
   },
